758c3a9a3ae78bbffcb10db2d244214fe569c7a0
Christopher Hall <chall361@gmail.com>
2019-11-13 19:27:18 +0000

    edited the return policy view

--- src/js/views/returnpolicy.js
+++ src/js/views/returnpolicy.js
@@ -3,9 +3,13 @@ export class ReturnPolicy extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Return Policy</h1>
+				<h1>
+					<u>Return Policy</u>
+				</h1>
 				<div className="jumbotron">
-					<h1 className="display-4">ALL SALES ARE FINAL!</h1>
+					<h1 className="display-4">
+						<u>ALL SALES ARE FINAL!</u>
+					</h1>
 					<p className="lead" />
 					<hr className="my-4" />
 					<p />
